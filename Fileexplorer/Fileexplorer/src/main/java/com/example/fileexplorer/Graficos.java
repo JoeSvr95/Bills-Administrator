062d4ff51618d24e9a695c0ca5fc1ebe7aaacb5f
JoeSvr95 <joesaverio@hotmail.com>
2016-12-01 00:59:37 -0500

    Toast
    Al presionar una partición del chart, éste muestra un Toast.

--- Fileexplorer/Fileexplorer/src/main/java/com/example/fileexplorer/Graficos.java
+++ Fileexplorer/Fileexplorer/src/main/java/com/example/fileexplorer/Graficos.java
@@ -4,12 +4,18 @@ import android.app.Activity;
 import android.graphics.Color;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
 import com.github.mikephil.charting.charts.PieChart;
 import com.github.mikephil.charting.components.Description;
 import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.PieData;
 import com.github.mikephil.charting.data.PieDataSet;
 import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.highlight.Highlight;
+import com.github.mikephil.charting.listener.OnChartValueSelectedListener;
 
 import java.util.ArrayList;
 
@@ -17,6 +23,7 @@ public class Graficos extends Activity {
 
     private ArrayList<Float> alimentos, vivienda, medicina, educacion, vestimenta, otros;
     private String[] xData = {"Alimentos", "Vivienda", "Medicina", "Educacion", "Vestimenta", "Otros"};
+	private ArrayList<PieEntry> yEntrys;
     PieChart pieChart;
 
     @Override
@@ -48,6 +55,28 @@ public class Graficos extends Activity {
         pieChart.setCenterTextSize(10);
 
         addDataSet(pieChart);
+		
+		pieChart.setOnChartValueSelectedListener(new OnChartValueSelectedListener() {
+            @Override
+            public void onValueSelected(Entry e, Highlight h) {
+                String compra = e.toString().substring(17);
+
+                int pos1 = 0;
+                for (int i = 0; i < yEntrys.size(); i++){
+                    if (yEntrys.get(i).getValue() == Float.parseFloat(compra)){
+                        pos1 = i;
+                    }
+                }
+
+                String categoria = xData[pos1];
+                Toast.makeText(Graficos.this, categoria + ": $" + compra, Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onNothingSelected() {
+
+            }
+        });
     }
 
     public void addDataSet(PieChart chart){
@@ -59,7 +88,7 @@ public class Graficos extends Activity {
         Float vesData = 0.0f;
         Float oData = 0.0f;
 
-        ArrayList<PieEntry> yEntrys = new ArrayList<PieEntry>();
+        yEntrys = new ArrayList<PieEntry>();
 
         for (Float data : alimentos){ alData += data; }
         yEntrys.add(new PieEntry(alData));
